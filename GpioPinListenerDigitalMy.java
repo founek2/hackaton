ffa327eeda704d048bf22b5081b6e3f988639c75
lucky <skalicky.martas@seznam.cz>
2017-04-07 23:24:49 +0200

    commit

--- GpioPinListenerDigitalMy.java
+++ GpioPinListenerDigitalMy.java
@@ -1,14 +1,13 @@
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
-import java.util.TimerTask;
-
 /**
  * Created by lucky on 4/7/17.
  */
 public class GpioPinListenerDigitalMy implements GpioPinListenerDigital {
     Light light;
     Thread thread;
+    boolean clicked = false;
 
     public GpioPinListenerDigitalMy(Light light) {
         this.light = light;
@@ -18,20 +17,21 @@ public class GpioPinListenerDigitalMy implements GpioPinListenerDigital {
     public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
         if (event.getState().isHigh()) {
             light.setLighted(true);
-            thread.interrupt();
+            clicked = true;
         } else {
             System.out.println("start timer " + light.getId());
-
-            thread=new Thread() {
+            clicked = false;
+            thread = new Thread() {
                 public void run() {
                     try {
-                        this.sleep(5000);
-                        System.out.println("trigered " + light.getId());
-                        light.setLighted(false);
-                    } catch (InterruptedException | IllegalThreadStateException e) {
+                        this.sleep(500);
+                        if (!clicked) {
+                            System.out.println("trigered " + light.getId());
+                            light.setLighted(false);
+                        }
+                    } catch (InterruptedException  e) {
                         e.printStackTrace();
                     }
-                    this.interrupt();
                 }
             };
             thread.start();
