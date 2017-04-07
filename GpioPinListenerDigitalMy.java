14f2e33bebfbe14573136255ffc1df08114c3eef
lucky <skalicky.martas@seznam.cz>
2017-04-07 22:37:06 +0200

    commit

--- GpioPinListenerDigitalMy.java
+++ GpioPinListenerDigitalMy.java
@@ -1,22 +1,41 @@
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
-import com.pi4j.wiringpi.SoftPwm;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by lucky on 4/7/17.
  */
 public class GpioPinListenerDigitalMy implements GpioPinListenerDigital {
     Light light;
-    public GpioPinListenerDigitalMy(Light light){
-        this.light=light;
+
+    Timer timer = null;
+
+    public GpioPinListenerDigitalMy(Light light) {
+        this.light = light;
     }
+
     @Override
     public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
         if (event.getState().isHigh()) {
-            light.setLighted(true);
+            if (timer == null) {
+                light.setLighted(true);
+            }else {
+                timer.cancel();
+                timer=null;
+            }
+
         } else {
-            light.setLighted(false);
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    light.setLighted(false);
+                    timer = null;
+                }
+            }, 0, 500);
         }
     }
 }
