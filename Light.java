c07e9d4a24d308865910d0ba56759885fa8478f8
lucky <skalicky.martas@seznam.cz>
2017-04-07 21:58:58 +0200

    commit

--- Light.java
+++ Light.java
@@ -14,19 +14,19 @@ public class Light {
     private GpioPinDigitalInput button;
     private boolean lighted = false;
     private LinkedList<Light[]> sideLights = new LinkedList<Light[]>();
-    private Map<Light, Integer> parrentLights = new HashMap<Light,Integer>();
+    private Map<Light, Integer> parrentLights = new HashMap<Light, Integer>();//mostFaraway to Least Faraway
 
-    public Light(int id, Pin led, Pin button){
-        this.id=id;
+    public Light(int id, Pin led, Pin button) {
+        this.id = id;
         this.led = GpioFactory.getInstance().provisionDigitalOutputPin(led);
         this.button = GpioFactory.getInstance().provisionDigitalInputPin(button, PinPullResistance.PULL_DOWN);
-        SoftPwm.softPwmCreate(this.led.getPin().getAddress(),0,100);
-        SoftPwm.softPwmWrite(this.led.getPin().getAddress(),LightLevels.LIGHT_LEVELS[0]);
+        SoftPwm.softPwmCreate(this.led.getPin().getAddress(), 0, 100);
+        SoftPwm.softPwmWrite(this.led.getPin().getAddress(), LightLevels.LIGHT_LEVELS[0]);
     }
 
-    public void addSideLight(Light[] sideLight){
-        if (sideLight.length!=LightLevels.LIGHT_LEVELS.length-2){
-            throw new IllegalArgumentException("There mus't be "+(LightLevels.LIGHT_LEVELS.length-2)+" side lights");
+    public void addSideLight(Light[] sideLight) {
+        if (sideLight.length != LightLevels.LIGHT_LEVELS.length - 2) {
+            throw new IllegalArgumentException("There mus't be " + (LightLevels.LIGHT_LEVELS.length - 2) + " side lights");
         }
         this.sideLights.add(sideLight);
     }
@@ -35,15 +35,43 @@ public class Light {
         return lighted;
     }
 
-    public void setSubLevelLight(Light parrent, int level){
-
+    public void addSubLevelLight(Light parrent, int level) {
+        parrentLights.put(parrent,level);
+        SoftPwm.softPwmWrite(led.getPin().getAddress(), maxLight());
     }
 
 
     public void setLighted(boolean lighted) {
         this.lighted = lighted;
-        if (lighted){
-            SoftPwm.softPwmWrite(led.getPin().getAddress(),LightLevels.LIGHT_LEVELS[LightLevels.LIGHT_LEVELS.length-1]);
+        if (lighted) {
+            SoftPwm.softPwmWrite(led.getPin().getAddress(), LightLevels.LIGHT_LEVELS[LightLevels.LIGHT_LEVELS.length - 1]);
+            for (Light[] lights : sideLights) {
+                for (int i = 0; i < lights.length; i++) {
+                    lights[i].addSubLevelLight(this,LightLevels.LIGHT_LEVELS[i+1]);
+                }
+            }
+        } else {
+            SoftPwm.softPwmWrite(led.getPin().getAddress(), maxLight());
+            for (Light[] lights : sideLights) {
+                for (int i = 0; i < lights.length; i++) {
+                    lights[i].removeSubLevelLight(this);
+                }
+            }
+        }
+    }
+
+    public int maxLight() {
+        Map.Entry<Light, Integer> maxEntry = null;
+
+        for (Map.Entry<Light, Integer> entry : parrentLights.entrySet()) {
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                maxEntry = entry;
+            }
+        }
+        if (maxEntry == null) {
+            return LightLevels.LIGHT_LEVELS[0];
+        } else {
+            return maxEntry.getValue();
         }
     }
 
